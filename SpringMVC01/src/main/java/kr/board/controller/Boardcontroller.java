8d91e160046b3b2d006ea4180e72034efd797e55
admin <admin@LAPTOP-4OM8PK7M>
2022-11-28 17:11:39 +0900

    commit

--- SpringMVC01/src/main/java/kr/board/controller/Boardcontroller.java
+++ SpringMVC01/src/main/java/kr/board/controller/Boardcontroller.java
@@ -3,34 +3,66 @@ package kr.board.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.board.entity.Board;
+import kr.board.mapper.BoardMapper;
 
 @Controller
 public class Boardcontroller {
 
-	// /boardList.do
+	// boardList.do
+	// 자동 주입 특정객체를 필요할때 찾아주는 의존성 주입
+	@Autowired
+	private BoardMapper mapper;
 	//HandlerMapping
 	@RequestMapping("/boardList.do")
 	public String boardList(Model model) {
 		
-		Board VO = new Board();
-		VO.setIdx(1);
-		VO.setTitle("게시판실습");
-		VO.setContent("게시판실습");
-		VO.setWriter("김민재");
-		VO.setIndate("2022-11-22");
-		VO.setCount(0);
-		
-		List<Board> list = new ArrayList<Board>();
-		list.add(VO);
-		list.add(VO);
-		list.add(VO);
+		List<Board> list = mapper.getLists();
 		model.addAttribute("list" , list);
 		return "boardList" ; // /WEB-INF/views/~.jsp (servlet-context) -> forward
 	}
-	
+	@GetMapping("/boardForm.do")
+	public String boardForm() {
+		return "boardForm";
+	}
+	@PostMapping("/boardInsert.do")
+	public String boardInsert(Board vo) { //파라미터 수집(Board)
+		mapper.boardInsert(vo);
+		return "redirect:/boardList.do";
+	}
+	@GetMapping("/boardContent.do")
+	public String boardContent(@RequestParam("idx") int idx, Model model) {
+		Board vo=mapper.boardContent(idx);
+		
+		mapper.boardCount(idx);
+		model.addAttribute("vo", vo);
+		
+		return "boardContent";
+	}
+	@GetMapping("/boardDelete.do/{idx}")
+	public String boardDelete(@PathVariable("idx") int idx) {
+		mapper.boardDelete(idx);
+		return "redirect:/boardList.do";
+	}
+	@GetMapping("/boardUpdateForm.do/{idx}")
+	public String boardUpdateForm(@PathVariable("idx") int idx, Model model) {
+		Board vo=mapper.boardContent(idx);
+		model.addAttribute("vo" , vo);
+		return "boardUpdate";
+	}
+	@PostMapping("boardUpdate.do")
+	public String boardUpdate(Board vo) {
+		mapper.boardUpdate(vo);
+		return "redirect:/boardList.do";
+		
+	}
 }
